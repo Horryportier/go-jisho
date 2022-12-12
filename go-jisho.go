8206c25fcf460c8ae399b5fc2026dabb3c9f4ccf
Horryportier <kowalskis354@gmail.com>
2022-12-12 19:03:16 +0100

    status func

--- go-jisho.go
+++ go-jisho.go
@@ -6,53 +6,53 @@ import (
 	"net/http"
 )
 
-
 const (
 	api string = "https://jisho.org/api/v1/search/words?keyword="
 )
 
 // takes word as key and returns data and error
 func Search(key string) (Word, error) {
-	var word Word 
+	var word Word
 
 	url := func() string {
 		return api + key
 	}()
 
-        resp, err :=  http.Get(url)
-        if err != nil {
-                return data, err 
-        }
-        body, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-                return data, err
-        }
+	resp, err := http.Get(url)
+	if err != nil {
+		return data, err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return data, err
+	}
 
-        json.Unmarshal([]byte(body), &word)
+	json.Unmarshal([]byte(body), &word)
 
 	return word, nil
 }
 
 // gets indexes and returns all japanese kanji and writing
-func (word Word) TransJapan(index ...int) ([]Japanese) {
-        var data Data
-        data = word.Data
-        var japanes []Japanese
-        for _,val := range index{
-        japanes = append(japanes, data.Data[val].Japanese...)
-        }
-        return japanes
+func (word Word) TransJapan(index ...int) []Japanese {
+	var data Data
+	data = word.Data
+	var japanes []Japanese
+	for _, val := range index {
+		japanes = append(japanes, data.Data[val].Japanese...)
+	}
+	return japanes
 }
 
-func (word Word) EngDefinition(index ...int) ([]Senses) {
-        var data Data
-        data = word.Data
-        var senses []Senses
-        for _,val := range index{
-                senses = append(senses, data.Data[val].Senses...)
-        }
-        return senses
+func (word Word) EngDefinition(index ...int) []Senses {
+	var data Data
+	data = word.Data
+	var senses []Senses
+	for _, val := range index {
+		senses = append(senses, data.Data[val].Senses...)
+	}
+	return senses
 }
 
-
-
+func (word Word) Status() int {
+	return word.Meta.Status
+}
