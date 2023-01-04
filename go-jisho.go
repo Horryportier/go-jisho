4fcb0f31e9d210790cf46beb6439973fbc6b3333
Horryportier <kowalskis354@gmail.com>
2023-01-04 16:39:00 +0100

    search teaks key and isEng val

--- go-jisho.go
+++ go-jisho.go
@@ -13,15 +13,19 @@ const (
 	api string = "https://jisho.org/api/v1/search/words?keyword="
 )
 
-func GetUrl(key string) (string) {
-        romaji := kana.KanaToRomaji(key)
-        return api+romaji
+func GetUrl(key string) string {
+	return api + key
 }
+
 // takes word as key and returns data and error
-func Search(key string) (Word, error) {
+func Search(key string, isEng bool) (Word, error) {
 	var word Word
 
-	url := GetUrl(key) 
+	if isEng {
+		key = kana.KanaToRomaji(key)
+	}
+	url := GetUrl(key)
+
 	resp, err := http.Get(url)
 	if err != nil {
 		return word, err
@@ -30,7 +34,7 @@ func Search(key string) (Word, error) {
 	if err != nil {
 		return word, err
 	}
-        defer resp.Body.Close() 
+	defer resp.Body.Close()
 
 	json.Unmarshal([]byte(body), &word)
 
@@ -39,24 +43,24 @@ func Search(key string) (Word, error) {
 
 // gets  indexes and returs respective items
 func (word Word) GetEntries(index ...int) []Data {
-        var data []Data
-        for _, val := range index {
-                if val >= word.Len() {
-                        log.Fatal("index out of range")
-                }
-                data = append(data, word.Data[val])
-        }
-        return data
+	var data []Data
+	for _, val := range index {
+		if val >= word.Len() {
+			log.Fatal("index out of range")
+		}
+		data = append(data, word.Data[val])
+	}
+	return data
 }
 
 // gets indexes and returns all japanese kanji and writing
 func (word Word) TransJapan(index ...int) []Japanese {
 	var japanes []Japanese
 	for _, val := range index {
-                if val >= word.Len() {
-                        log.Fatal("index out of range")
-                }
-                japanes = append(japanes, word.Data[val].Japanese...)
+		if val >= word.Len() {
+			log.Fatal("index out of range")
+		}
+		japanes = append(japanes, word.Data[val].Japanese...)
 	}
 	return japanes
 }
@@ -65,24 +69,24 @@ func (word Word) TransJapan(index ...int) []Japanese {
 func (word Word) EngDefinition(index ...int) []Senses {
 	var senses []Senses
 	for _, val := range index {
-                if val >= word.Len() {
-                        log.Fatal("index out of range")
-                }
-                senses = append(senses, word.Data[val].Senses...)
-        }
+		if val >= word.Len() {
+			log.Fatal("index out of range")
+		}
+		senses = append(senses, word.Data[val].Senses...)
+	}
 	return senses
 }
 
 //Gets eng Jlpt every item in data
 func (word Word) Jlpt(index ...int) []string {
-        var jlpt []string
-        for _, val := range index {
-                if val >= word.Len() {
-                        log.Fatal("index out of range")
-                }
-                jlpt = append(jlpt, word.Data[val].Jlpt...)
-        }
-        return jlpt
+	var jlpt []string
+	for _, val := range index {
+		if val >= word.Len() {
+			log.Fatal("index out of range")
+		}
+		jlpt = append(jlpt, word.Data[val].Jlpt...)
+	}
+	return jlpt
 }
 
 func (word Word) Status() int {
@@ -90,6 +94,5 @@ func (word Word) Status() int {
 }
 
 func (word Word) Len() int {
-        return len(word.Data)
+	return len(word.Data)
 }
-
